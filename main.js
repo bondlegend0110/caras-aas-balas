9eb8d68b66bb1a517385febd943e6e2a550fd92d
bondlegend0110 <jose_sandoval@brown.edu>
2023-12-15 10:35:13 -0500

    pretty sure i make a g42 appear

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ $(document).ready(function() {
     var flipbook = $("#flipbook");
 
     flipbook.turn({
-        width: 1000,
-        height: 800,
         autoCenter: true
     });
 
+    function resizeFlipbook() {
+        // Calculate available height (viewport height minus the height of the buttons)
+        var availableHeight = $(window).height() - $("#prev-button").outerHeight(true) - $("#next-button").outerHeight(true);
+        var availableWidth = $(window).width();
+
+        // Adjust flipbook size
+        flipbook.turn('size', availableWidth, availableHeight);
+    }
+
+    // Resize the flipbook on window resize
+    $(window).resize(resizeFlipbook);
+
+    // Initial resize
+    resizeFlipbook();
     // Next button
     $("#next-button").click(function() {
         flipbook.turn("next");
